2c8b5829ddcc6c37f3b4fbaba66e89123a32d386
Aaron A. Rich <aaron@aaronarich.com>
2018-02-19 10:31:43 -0500

    Create FeaturedMix and Header components

---
+++ src/components/FeaturedMix.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const FeaturedMix = props => (
+  <div>
+    <p>Featured Mix</p>
+    <h1>Mix Name</h1>
+    {/* Play button */}
+  </div>
+);
+
+export default FeaturedMix;
