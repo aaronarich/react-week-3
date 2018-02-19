46c5d09fd354d6e2e04bdfc2806cb2a339c5b1f6
Aaron A. Rich <aaron@aaronarich.com>
2018-02-19 10:32:01 -0500

    Import FeaturedMix and Header components into app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import FeaturedMix from "./FeaturedMix";
+import Header from "./Header";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
+      <div>
+        <div>
+          <FeaturedMix />
+          <div>
+            {/* Header */}
+            {/* Routed page */}
+          </div>
+        </div>
+        {/* Audio player */}
       </div>
     );
   }
