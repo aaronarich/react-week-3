ba87ae6840559e16aba3a854a4e3e5884e2dca11
Aaron A. Rich <aaron@aaronarich.com>
2018-02-19 14:41:44 -0500

    Add styles to properly display player on bottom and FeaturedMix on left

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div>
+        <div className="flex-l justify-end">
           <FeaturedMix />
           <div>
             <Header />
@@ -19,6 +19,7 @@ class App extends Component {
           height="60"
           src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FGold_Flake_Paint%2Fgfp-mix-january-2018%2F"
           frameBorder="0"
+          className="z-5 db fixed bottom-0"
         />
       </div>
     );
