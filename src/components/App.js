391ae78271992ef70a2811a77d843517ab429e51
Aaron A. Rich <aaron@aaronarich.com>
2018-02-19 14:29:18 -0500

    Update mix to one that's a bit easier to listen to

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ class App extends Component {
           title="Mixcloud Player"
           width="100%"
           height="60"
-          src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FGold_Flake_Paint%2Fsunday-slowdown-episode-eighteen%2F"
+          src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FGold_Flake_Paint%2Fgfp-mix-january-2018%2F"
           frameBorder="0"
         />
       </div>
