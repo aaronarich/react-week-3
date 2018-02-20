a37dfeb1319e19afd9991cada0630bda0124b1fb
Aaron A. Rich <aaron@aaronarich.com>
2018-02-20 18:10:35 -0500

    Update Nav links to NavLink (React Router)

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,39 @@
 import React, { Component } from "react";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import FeaturedMix from "./FeaturedMix";
 import Header from "./Header";
+
+const Home = () => <h1>Home</h1>;
+const Archive = () => <h1>Archive</h1>;
+const About = () => <h1>About</h1>;
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <div className="flex-l justify-end">
-          <FeaturedMix />
-          <div className="w-50-l relative z-1">
-            <Header />
-            {/* Routed page */}
+      <Router>
+        <div>
+          <div className="flex-l justify-end">
+            <FeaturedMix />
+            <div className="w-50-l relative z-1">
+              <Header />
+              {/* Routed page */}
+              <Route exact path="/" component={Home} />
+              <Route path="/archive" component={Archive} />
+              <Route path="/about" component={About} />
+            </div>
           </div>
+          <iframe
+            title="Mixcloud Player"
+            width="100%"
+            height="60"
+            src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FGold_Flake_Paint%2Fgfp-mix-january-2018%2F"
+            frameBorder="0"
+            className="z-5 db fixed bottom-0"
+          />
         </div>
-        <iframe
-          title="Mixcloud Player"
-          width="100%"
-          height="60"
-          src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FGold_Flake_Paint%2Fgfp-mix-january-2018%2F"
-          frameBorder="0"
-          className="z-5 db fixed bottom-0"
-        />
-      </div>
+      </Router>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,34 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = props => (
   <header className="black mb5 pt5">
     <h1 className="ttu f3 tracked-mega anton tc mt0 mb3">Marmalade.fm</h1>
     <ul className="list flex justify-center pl0">
       <li className="mh2">
-        <a className="nav-link link biryani-black f6 ttu gray" href="#">
+        <NavLink
+          exact
+          to="/"
+          className="nav-link link biryani-black f6 ttu gray"
+        >
           What's hot
-        </a>
+        </NavLink>
       </li>
       <li className="mh2">
-        <a className="nav-link link biryani-black f6 ttu gray" href="#">
+        <NavLink
+          to="/archive"
+          className="nav-link link biryani-black f6 ttu gray"
+        >
           Archive
-        </a>
+        </NavLink>
       </li>
       <li className="mh2">
-        <a className="nav-link link biryani-black f6 ttu gray" href="#">
+        <NavLink
+          to="/about"
+          className="nav-link link biryani-black f6 ttu gray"
+        >
           About
-        </a>
+        </NavLink>
       </li>
     </ul>
   </header>
