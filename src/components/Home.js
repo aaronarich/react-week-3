197fd86f05cc10b0d3e4b12c4dac5c24d9c796fc
Aaron A. Rich <aaron@aaronarich.com>
2018-02-26 12:27:00 -0500

    Remove placeholder mixes in order to pull dynamic mix data

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,29 +3,11 @@ import Mix from "./Mix";
 
 const Home = props => (
   <div className="flex flex-wrap justify-between mixes ph3 ph4-l">
-    <div className="mix mb4">
-      <Mix
-        name="Sunday Slowdown 15"
-        id="/Gold_Flake_Paint/sunday-slowdown-episode-fifteen/"
-        {...props}
-      />
-    </div>
-
-    <div className="mix mb4">
-      <Mix
-        name="Sunday Slowdown 16"
-        id="/Gold_Flake_Paint/sunday-slowdown-episode-sixteen/"
-        {...props}
-      />
-    </div>
-
-    <div className="mix mb4">
-      <Mix
-        name="Sunday Slowdown 17"
-        id="/Gold_Flake_Paint/sunday-slowdown-episode-seventeen/"
-        {...props}
-      />
-    </div>
+    {props.mix && (
+      <div className="mix mb4">
+        <Mix {...props} {...props.mix} />
+      </div>
+    )}
   </div>
 );
 
