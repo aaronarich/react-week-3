785c250fdc4930ac149620344e73e529282f4358
Aaron A. Rich <aaron@aaronarich.com>
2018-02-26 11:20:55 -0500

    Create PlayMix component to wrap child mixes in

--- src/components/Mix.js
+++ src/components/Mix.js
@@ -1,20 +1,18 @@
 import React from "react";
 import PlayButton from "./PlayButton";
+import PlayMix from "./PlayMix";
 
-const Mix = ({ name, playMix, currentMix }) => (
-  <div
-    className="aspect-ratio aspect-ratio--3x4 pointer bg-black"
-    onClick={() =>
-      playMix("/Gold_Flake_Paint/sunday-slowdown-episode-fifteen/")
-    }
-  >
-    <div className="ph3 pv4 aspect-ratio--object mix-overlay">
-      <div className="flex items-center relative z-2">
-        <h1 className="f4 f3-l mv0 white ttu biryani pr2 lh-title">{name}</h1>
-        <PlayButton />
-        {/* PlayButton goes here */}
+const Mix = ({ name, ...props }) => (
+  <div className="aspect-ratio aspect-ratio--3x4 pointer bg-black">
+    <PlayMix {...props}>
+      <div className="ph3 pv4 aspect-ratio--object mix-overlay">
+        <div className="flex items-center relative z-2">
+          <h1 className="f4 f3-l mv0 white ttu biryani pr2 lh-title">{name}</h1>
+          <PlayButton />
+          {/* PlayButton goes here */}
+        </div>
       </div>
-    </div>
+    </PlayMix>
   </div>
 );
 
