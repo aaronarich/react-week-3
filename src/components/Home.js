570cd8b3e01884f619a71a3c08fdc9d21ab9e2ac
Aaron A. Rich <aaron@aaronarich.com>
2018-02-26 11:20:07 -0500

    Add a few more mixes to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,27 @@ import Mix from "./Mix";
 const Home = props => (
   <div className="flex flex-wrap justify-between mixes ph3 ph4-l">
     <div className="mix mb4">
-      <Mix {...props} />
+      <Mix
+        name="Sunday Slowdown 15"
+        id="/Gold_Flake_Paint/sunday-slowdown-episode-fifteen/"
+        {...props}
+      />
+    </div>
+
+    <div className="mix mb4">
+      <Mix
+        name="Sunday Slowdown 16"
+        id="/Gold_Flake_Paint/sunday-slowdown-episode-sixteen/"
+        {...props}
+      />
+    </div>
+
+    <div className="mix mb4">
+      <Mix
+        name="Sunday Slowdown 17"
+        id="/Gold_Flake_Paint/sunday-slowdown-episode-seventeen/"
+        {...props}
+      />
     </div>
   </div>
 );
