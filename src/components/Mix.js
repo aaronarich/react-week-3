a83c220b8379f675da9d998b4bfcfaa5df6727a6
Aaron A. Rich <aaron@aaronarich.com>
2018-03-17 13:31:39 -0400

    Pass mix slug from Mix to Show component to create individual show page

--- src/components/Mix.js
+++ src/components/Mix.js
@@ -1,20 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PlayButton from "./PlayButton";
 import PlayMix from "./PlayMix";
 
-const Mix = ({ name, pictures, ...props }) => (
+const Mix = ({ name, pictures, slug, ...props }) => (
   <div
     className="aspect-ratio aspect-ratio--3x4 pointer bg-black bg-center cover"
     style={{ backgroundImage: `url(${pictures.extra_large})` }}
   >
-    <PlayMix {...props}>
-      <div className="ph3 pv4 aspect-ratio--object mix-overlay">
-        <div className="flex items-center relative z-2">
-          <h1 className="f4 f3-l mv0 white ttu biryani pr2 lh-title">{name}</h1>
-          <PlayButton />
+    <Link to={`/show/${slug}`}>
+      <PlayMix {...props}>
+        <div className="ph3 pv4 aspect-ratio--object mix-overlay">
+          <div className="flex items-center relative z-2">
+            <h1 className="f4 f3-l mv0 white ttu biryani pr2 lh-title">
+              {name}
+            </h1>
+            <PlayButton />
+          </div>
         </div>
-      </div>
-    </PlayMix>
+      </PlayMix>
+    </Link>
   </div>
 );
 
